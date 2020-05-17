063f693f20cf99e7cf18e185677b508b908ba4cd
unknown <friedrichtydecks@gmail.com>
2020-05-17 21:03:39 +0200

    added 1d

--- src/main.java
+++ src/main.java
@@ -1,33 +1,45 @@
+import java.util.Arrays;
 
 public class main {
 
 	public static void main(String[] args) {
 		int[] a = { 1, 2, 3, 4, -1, -2, -3, -4 };
-		int[] a1;
-		int[] a2 = new int[a.length / 2];
-		if (a.length % 2 != 0) 
-		{
-			a1 = new int[a.length / 2 + 1];
-		} else 
-		{
-			a1 = new int[a.length / 2];
-		}
-		for(int i=0;i<a.length;i++)
-		{
-			if(i % 2 == 0)
-			{
-				a1[i/2]=a[i];
-			}
-			else
-			{
-				a2[i/2]=a[i];
-			}
-		}
+		int[] b1 = getEvenArrayFrom(a);
+		int[] b2 = getOddArrayFrom(a);
 		
 		System.out.println(java.util.Arrays.toString(a));
-		System.out.println(java.util.Arrays.toString(a1));
-		System.out.println(java.util.Arrays.toString(a2));
+		System.out.println(java.util.Arrays.toString(b1));
+		System.out.println(java.util.Arrays.toString(b2));
+	}
+	
+	
+	public static int[] getOddArrayFrom(int[] a)
+	{
+		int[] b2 = new int[a.length];
+		int lengthb = 0;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i] % 2 != 0)
+			{
+				b2[lengthb++] = a[i];
+			}
+		}
+		return Arrays.copyOfRange(b2, 0, lengthb);
+	}
 
+	
+	public static int[] getEvenArrayFrom(int[] a)
+	{
+		int[] b1 = new int[a.length];
+		int lengthb = 0;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i] % 2 == 0)
+			{
+				b1[lengthb++] = a[i];
+			}
+		}
+		return Arrays.copyOfRange(b1, 0, lengthb);
 	}
 
 }
